5849df0dcec162f7fbd47c172a636137ad03410e
Joseph Chretien-Golden <joseph@zuul.com>
2020-10-21 11:45:37 -0500

    adds router links

--- resources/js/components/Example2.vue
+++ resources/js/components/Example2.vue
@@ -3,9 +3,11 @@
         <div class="row justify-content-center">
             <div class="col-md-8">
                 <div class="card">
-                    <div class="card-header">Example Component</div>
+                    <div class="card-header">Second Component</div>
 
-                    <div class="card-body">I'm an example component.</div>
+                    <div class="card-body">
+                        I'm a second component.
+                    </div>
                 </div>
             </div>
         </div>
